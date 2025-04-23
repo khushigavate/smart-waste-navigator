afacb8d506516f65cb578da43abca7c2b31b9fef
khushi <error404khushi@gmail.com>
2025-04-24 00:03:35 +0530

    Fix DB config to use Railway env var names (MYSQL_*) in db.js

--- backend/db.js
+++ backend/db.js
@@ -1,16 +1,12 @@
 // backend/db.js
-require('dotenv').config();
-console.log('Connecting to DB with user=', process.env.DB_USER);
-
-// rest of your pool setup…
-
 const mysql = require('mysql2/promise');
-const pool = mysql.createPool({
-  host:     process.env.DB_HOST,
-  user:     process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
+const pool  = mysql.createPool({
+  host:     process.env.MYSQL_HOST,
+  port:     process.env.MYSQL_PORT,          // <-- include port!
+  user:     process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
   waitForConnections: true,
-  connectionLimit:    10
+  connectionLimit: 10,
 });
 module.exports = pool;
